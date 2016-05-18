4149d3976447183de406e3dae47c52604379d0e3
UCDTtbot <tylwelsh@ucdavis.edu>
2016-05-18 10:41:04 -0700

    Updated Camera Move

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,39 +6,52 @@ public class GameController : MonoBehaviour {
     [SerializeField]
     private Camera MainCamera;
     [SerializeField]
-    private Transform Test_Start_Pos;
+    private Transform Init_Pos;
     [SerializeField]
-    private Transform Test_End_Pos;
+    private Transform[] Camera_Pos = new Transform[4];
     [SerializeField]
+    private float Camera_Move_Time = 2.0f;
+    [SerializeField]
+    private GameObject MainMenu;
+
+
+    private float waitTime = 1.0f;
+    private int curPos = -1;
     private Vector3 Camera_Move_Velocity = Vector3.zero;
-    [SerializeField]
-    private float Camera_Move_Speed = 2.0f;
 
-
-    private float waitTime = 5;
-
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
-        MainCamera.transform.position = Test_Start_Pos.transform.position;
+        MainMenu.SetActive(true);
+        MainCamera.transform.position = Init_Pos.transform.position;
+        curPos = 0;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (waitTime <= 0)
-            MainCamera.transform.position = Vector3.SmoothDamp(MainCamera.transform.position, Test_End_Pos.position, ref Camera_Move_Velocity, Camera_Move_Speed);
-        else
-            waitTime--;
-
-
-        //void Update() {
-        //    float yAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, target.eulerAngles.y, ref yVelocity, smooth);
-        //    Vector3 position = target.position;
-        //    position += Quaternion.Euler(0, yAngle, 0) * new Vector3(0, 0, -distance);
-        //    transform.position = position;
-        //    transform.LookAt(target);
-        //}
 
     }
+
+    public void StartGame()
+    {
+        curPos = 0;
+        MainMenu.SetActive(false);
+        StartCoroutine(MoveCamera(Camera_Pos[curPos], Camera_Move_Time));
+    }
+
+    public IEnumerator MoveCamera(Transform Next_Pos, float time)
+    {
+        float elapsedTime = 0;
+
+        while (elapsedTime < time)
+        {
+            //MainCamera.transform.position = Vector3.SmoothDamp(MainCamera.transform.position, Camera_Pos[curPos].position, ref Camera_Move_Velocity, (elapsedTime / time));
+            MainCamera.transform.position = Vector3.Lerp(MainCamera.transform.position, Next_Pos.position, (elapsedTime / time));
+            MainCamera.transform.rotation = Quaternion.Lerp(MainCamera.transform.rotation, Next_Pos.rotation, (elapsedTime / time));
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
 }
